dbb2de30523988411d8d05b8d005de899aab2051
Zoey <github@mangotec.co.uk>
2021-12-15 10:35:41 +0000

    add favicon

--- header.php
+++ header.php
@@ -16,6 +16,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+	<meta name="apple-mobile-web-app-title" content="Secula">
+	<meta name="application-name" content="Secula">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="theme-color" content="#ffffff">
 
 	<?php wp_head(); ?>
 </head>
